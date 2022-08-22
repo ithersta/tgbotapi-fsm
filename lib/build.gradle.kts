c1115261dddb391876472384b195ef83aab5d80d
ithersta <schilyaev.vlad@gmail.com>
2022-08-22 19:43:27 +0400

    Publish

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.7.10"
     `java-library`
+    `maven-publish`
 }
 
 version = "0.1.0"
@@ -12,3 +13,37 @@ repositories {
 dependencies {
     implementation("dev.inmo:tgbotapi:3.1.1")
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.ithersta.tgbotapi"
+            artifactId = "tgbotapi-fsm"
+            version = version
+
+            from(components["java"])
+
+            pom {
+                name.set("Telegram Bot API Finite State Machine DSL")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+            }
+
+            repositories {
+                maven {
+                    url = uri("https://repo.repsy.io/mvn/ithersta/tgbotapi")
+                    credentials {
+                        username = properties["repsyUsername"].toString()
+                        password = properties["repsyPassword"].toString()
+                    }
+                }
+            }
+        }
+    }
+}
+
+
